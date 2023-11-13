4612c2972b3cdcf6991079b35e0b446f3486363c
Bloic <loic.benos@gmail.com>
2023-11-13 22:56:22 +0100

    création de la page en savoir plus

---
+++ src/Controller/AboutMeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutMeController extends AbstractController
+{
+    #[Route('/about-me', name: 'app_about_me')]
+    public function index(): Response
+    {
+        return $this->render('about_me/index.html.twig', [
+        ]);
+    }
+}
